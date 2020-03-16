227137c8fe509c6365b5688fa96fff9d6d2705cb
Starashwin <bhatnagarashwin12@gmail.com>
2020-03-16 17:43:58 +0000

    Added extra run

--- Lab9/Main.java
+++ Lab9/Main.java
@@ -1,10 +1,15 @@
+import javax.swing.JOptionPane;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Invoker invoker = new Invoker();
 		MyCom myCom = new MyCom();
-		myCom.set_name("My Com");
+		myCom.set_name(JOptionPane.showInputDialog("Please enter Name"));
+		invoker.register(myCom);
+		invoker.run();
+		
+		myCom.set_name(JOptionPane.showInputDialog("Please enter Name"));
 		invoker.register(myCom);
 		invoker.run();
 	}
